3e57d931d9e597a6fc7c8c1f9269dba3363ae8b8
Ethan Brooks <ethanb98@vt.edu>
2018-11-13 17:50:09 -0500

    Partial add of the provided test

--- notebook_test.cpp
+++ notebook_test.cpp
@@ -16,6 +16,88 @@ void NotebookTest::initTestCase(){
 
 }
 
+void NotebookTest::testDiscretePlotLayout() {
+
+	std::string program = R"( 
+(discrete-plot (list (list -1 -1) (list 1 1)) 
+    (list (list "title" "The Title") 
+          (list "abscissa-label" "X Label") 
+          (list "ordinate-label" "Y Label") ))
+)";
+
+	inputWidget->setPlainText(QString::fromStdString(program));
+	QTest::keyClick(inputWidget, Qt::Key_Return, Qt::ShiftModifier);
+
+	auto view = outputWidget->findChild<QGraphicsView *>();
+	QVERIFY2(view, "Could not find QGraphicsView as child of OutputWidget");
+
+	auto scene = view->scene();
+
+	// first check total number of items
+	// 8 lines + 2 points + 7 text = 17
+	auto items = scene->items();
+	QCOMPARE(items.size(), 17);
+
+	// make them all selectable
+	foreach(auto item, items) {
+		item->setFlag(QGraphicsItem::ItemIsSelectable);
+	}
+
+	double scalex = 20.0 / 2.0;
+	double scaley = 20.0 / 2.0;
+
+	double xmin = scalex * -1;
+	double xmax = scalex * 1;
+	double ymin = scaley * -1;
+	double ymax = scaley * 1;
+	double xmiddle = (xmax + xmin) / 2;
+	double ymiddle = (ymax + ymin) / 2;
+
+	// check title
+	QCOMPARE(findText(scene, QPointF(xmiddle, -(ymax + 3)), 0, QString("The Title")), 1);
+
+	// check abscissa label
+	QCOMPARE(findText(scene, QPointF(xmiddle, -(ymin - 3)), 0, QString("X Label")), 1);
+
+	// check ordinate label
+	QCOMPARE(findText(scene, QPointF(xmin - 3, -ymiddle), -90, QString("Y Label")), 1);
+
+	// check abscissa min label
+	QCOMPARE(findText(scene, QPointF(xmin, -(ymin - 2)), 0, QString("-1")), 1);
+
+	// check abscissa max label
+	QCOMPARE(findText(scene, QPointF(xmax, -(ymin - 2)), 0, QString("1")), 1);
+
+	// check ordinate min label
+	QCOMPARE(findText(scene, QPointF(xmin - 2, -ymin), 0, QString("-1")), 1);
+
+	// check ordinate max label
+	QCOMPARE(findText(scene, QPointF(xmin - 2, -ymax), 0, QString("1")), 1);
+
+	// check the bounding box bottom
+	QCOMPARE(findLines(scene, QRectF(xmin, -ymin, 20, 0), 0.1), 1);
+
+	// check the bounding box top
+	QCOMPARE(findLines(scene, QRectF(xmin, -ymax, 20, 0), 0.1), 1);
+
+	// check the bounding box left and (-1, -1) stem
+	QCOMPARE(findLines(scene, QRectF(xmin, -ymax, 0, 20), 0.1), 2);
+
+	// check the bounding box right and (1, 1) stem
+	QCOMPARE(findLines(scene, QRectF(xmax, -ymax, 0, 20), 0.1), 2);
+
+	// check the abscissa axis
+	QCOMPARE(findLines(scene, QRectF(xmin, 0, 20, 0), 0.1), 1);
+
+	// check the ordinate axis 
+	QCOMPARE(findLines(scene, QRectF(0, -ymax, 0, 20), 0.1), 1);
+
+	// check the point at (-1,-1)
+	QCOMPARE(findPoints(scene, QPointF(-10, 10), 0.6), 1);
+
+	// check the point at (1,1)
+	QCOMPARE(findPoints(scene, QPointF(10, -10), 0.6), 1);
+}
 
 QTEST_MAIN(NotebookTest)
 #include "notebook_test.moc"
